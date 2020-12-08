ef79f022f8404e14cbb91d4caf95f9e6e9a31d05
Brianna Tanusi <btanusi@gmail.com>
2020-12-08 11:18:57 -0800

    completed test demonstrating, starting PI problem

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello world";
+    }
+
     @GetMapping("/")
     public String helloWorld() {
         return "Hello from Spring!";
+    }
+
+    @GetMapping("/tasks")
+    public String getTasks() {
+        return "These are tasks";
+    }
+
+    @PostMapping("/tasks")
+    public String createTask() {
+        return "You just POSTed to /tasks";
     }
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(com.example.demo.HelloController.class)
 class DemoApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	public void testIndexEndpoint() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/hello");
+		this.mvc.perform(request).andExpect(status().isOk()).andExpect(content().string("Hello world"));
 	}
 
 }
