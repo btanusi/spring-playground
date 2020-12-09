d8a2b9904cb54d43b17bc86fb9a8d2d39e79ac8d
Brianna Tanusi <btanusi@gmail.com>
2020-12-09 13:44:16 -0800

    added flight problem

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class HelloController {
@@ -110,6 +111,82 @@ public class HelloController {
         } else {
             return retType;
         }
+    }
+
+    Passenger passenger1;
+    Ticket ticket1;
+    Flight flight1;
+    private void setUpFlight1(){
+        passenger1 = new Passenger();
+        passenger1.setFirstName("Some name");
+        passenger1.setLastName("Some other name");
+        ticket1 = new Ticket();
+        ticket1.setPassenger(passenger1);
+        ticket1.setPrice(200);
+        flight1 = new Flight();
+        flight1.setDeparts(new Date(2017-1900, 3, 21, 14-7, 34));
+        Ticket[] tickets = new Ticket[]{ticket1};
+        flight1.setTickets(tickets);
+    }
+
+    @GetMapping("/flights/flight")
+    public Flight getFlight() {
+        this.setUpFlight1();
+        return flight1;
+    }
+
+    Passenger passenger2;
+    Ticket ticket2;
+    Flight flight2;
+    @GetMapping("/flights")
+    public List<Flight> getFlights(){
+        this.setUpFlight1();
+        passenger2 = new Passenger();
+        passenger2.setFirstName("Some other name");
+        ticket2 = new Ticket();
+        ticket2.setPassenger(passenger2);
+        ticket2.setPrice(400);
+        flight2 = new Flight();
+        flight2.setDeparts(new Date(2017-1900, 3, 21, 14-7, 34));
+        Ticket[] tickets = new Ticket[]{ticket2};
+        flight2.setTickets(tickets);
+        return Arrays.asList(flight1, flight2);
+    }
+
+    public static class Flight {
+        private Date departs;
+        private Ticket[] tickets;
+
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+        @JsonProperty("Departs")
+        public Date getDeparts(){ return departs; }
+        public void setDeparts(Date departs){ this.departs = departs; }
+        @JsonProperty("Tickets")
+        public Ticket[] getTickets() { return tickets; }
+        public void setTickets(Ticket[] tickets) { this.tickets = tickets; }
+    }
+
+    public static class Ticket {
+        private Passenger passenger;
+        private Integer price;
+        @JsonProperty("Passenger")
+        public Passenger getPassenger(){ return passenger; }
+        public void setPassenger(Passenger passenger) { this.passenger = passenger; }
+        @JsonProperty("Price")
+        public Integer getPrice(){ return price; }
+        public void setPrice(Integer price){ this.price = price; }
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class Passenger {
+        private String firstName;
+        private String lastName;
+        @JsonProperty("FirstName")
+        public String getFirstName() { return firstName; }
+        public void setFirstName(String firstName) { this.firstName = firstName; }
+        @JsonProperty("LastName")
+        public String getLastName() { return lastName; }
+        public void setLastName(String lastName) { this.lastName = lastName; }
     }
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.Option;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -8,8 +10,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Date;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(com.example.demo.HelloController.class)
 class DemoApplicationTests {
@@ -102,6 +109,70 @@ class DemoApplicationTests {
 				.param("height", "7");
 		;
 		this.mvc.perform(request).andExpect(status().isOk()).andExpect(content().string("Area of a 4x7 rectangle is 28"));
+	}
+
+	/*
+	{
+  "departs": "2017-04-21 14:34",
+  "tickets": [
+    {
+      "passenger": {
+        "firstName": "Some name",
+        "lastName": "Some other name"
+      },
+      "price": 200
+    }
+  ]
+}
+	 */
+	/*
+	@Test
+	public void testFlight() throws Exception {
+		this.mvc.perform(
+				MockMvcRequestBuilders
+				.get("/flights/flight")
+						.accept(MediaType.APPLICATION_JSON_UTF8)
+						.contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.departs", is("2017-04-21 14:34")))
+				.andExpect(jsonPath("$.tickets[0].passenger.firstName", is("Some name")))
+				.andExpect(jsonPath("$.tickets[0].passenger.lastName", is("Some other name")))
+				.andExpect(jsonPath("$.tickets[0].price", is(200)));
+	}
+
+	@Test
+	public void testListOfFlights() throws Exception {
+		this.mvc.perform(
+				MockMvcRequestBuilders
+						.get("/flights")
+						.accept(MediaType.APPLICATION_JSON_UTF8)
+						.contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].departs", is("2017-04-21 14:34")))
+				.andExpect(jsonPath("$[0].tickets[0].passenger.firstName", is("Some name")))
+				.andExpect(jsonPath("$[0].tickets[0].passenger.lastName", is("Some other name")))
+				.andExpect(jsonPath("$[0].tickets[0].price", is(200)))
+				.andExpect(jsonPath("$[1].departs", is("2017-04-21 14:34")))
+				.andExpect(jsonPath("$[1].tickets[0].passenger.firstName", is("Some other name")))
+				//.andExpect(jsonPath("$[1].tickets[0].passenger.lastName", is(null)))
+				.andExpect(jsonPath("$[1].tickets[0].price", is(400)));
+	}
+	*/
+	@Test
+	public void refactedTestListOfFlights() throws Exception {
+		this.mvc.perform(
+				MockMvcRequestBuilders
+						.get("/flights")
+						.accept(MediaType.APPLICATION_JSON_UTF8)
+						.contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].Departs", is("2017-04-21 14:34")))
+				.andExpect(jsonPath("$[0].Tickets[0].Passenger.FirstName", is("Some name")))
+				.andExpect(jsonPath("$[0].Tickets[0].Passenger.LastName", is("Some other name")))
+				.andExpect(jsonPath("$[0].Tickets[0].Price", is(200)))
+				.andExpect(jsonPath("$[1].Departs", is("2017-04-21 14:34")))
+				.andExpect(jsonPath("$[1].Tickets[0].Passenger.FirstName", is("Some other name")))
+				.andExpect(jsonPath("$[1].Tickets[0].Price", is(400)));
 	}
 
 }
